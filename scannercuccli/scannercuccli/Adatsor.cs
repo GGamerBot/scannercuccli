2eade7ecb2917b188cdb14d4f6742dddcab7a2f9
Petrik Diak <diak@petrik.hu>
2024-09-20 15:35:28 +0200

    Adatsor osztály létezik

---
+++ scannercuccli/scannercuccli/Adatsor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scannercuccli
+{
+	internal class Adatsor
+	{
+		string vonalkod;
+		DateTime idopont;
+		//bool belepE; TBA
+
+		//Be/kilépés rögzítése a belépés időpontjában
+		public Adatsor(string vonalkod)
+		{
+			this.vonalkod = vonalkod;
+			this.idopont = DateTime.Now;
+		}
+
+		//Be/kilépés utólagos rögzítésére
+		public Adatsor(string vonalkod, DateTime idopont)
+		{
+			this.vonalkod = vonalkod;
+			this.idopont = idopont;
+		}
+
+		public string Vonalkod { get => vonalkod; }
+		public DateTime Idopont { get => idopont; }
+
+		//CSV készítésére való!!
+		public override string ToString()
+		{
+
+			return $"{vonalkod};{idopont.Year}.{idopont.Month}.{idopont.Day};{idopont.Hour}:{idopont.Minute}:{idopont.Second}";
+		}
+
+	}
+}
--- scannercuccli/scannercuccli/ScannerProgram.cs
+++ scannercuccli/scannercuccli/ScannerProgram.cs
@@ -9,16 +9,22 @@ namespace scannercuccli
 	internal class ScannerProgram
 	{
 		static HashSet<string> scannedCodes = new HashSet<string>();
+		static List<Adatsor> adatbazis = new List<Adatsor>();
 
 		static void Main(string[] args)
 		{
 			while (true)
 			{
-				Console.WriteLine("Kérlek, scanneld be a kódot (vagy írj 'exit'-et a kilépéshez):");
+                Console.WriteLine("Kérlek, scanneld be a kódot (vagy írj 'exit'-et a kilépéshez):");
 				string code = Console.ReadLine();
 
 				if (code.ToLower() == "exit")
 				{
+					foreach (var adatsor in adatbazis)
+					{
+                        Console.WriteLine(adatsor);
+						//TBA: CSV file-ba írás
+                    }
 					break;
 				}
 
@@ -32,6 +38,8 @@ namespace scannercuccli
 					Console.WriteLine($"A {code} kódu diák belépett.");
 					scannedCodes.Add(code);  
 				}
+				adatbazis.Add(new Adatsor(code));
+
 			}
 		}
 	}
